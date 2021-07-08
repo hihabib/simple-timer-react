cfa68c195ea2d63d5b60e2117b4c259c80085e05
Habib <habibulislam6862@gmail.com>
2021-07-09 00:00:57 +0600

    increment and decrement added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
 import React, {Component} from 'react'
 import './App.css'
 class App extends Component {
+    state = {
+        count: 0
+    }
+    handleIncrement = () => {
+        this.setState((el)=>{
+            return(
+                {
+                    count: el.count + 1
+                }
+            )
+        });
+    }
+    handleDecrement = () => {
+        if( this.state.count > 0 ){
+            this.setState((el)=>{
+                return(
+                    {
+                        count: el.count - 1
+                    }
+                )
+            });
+        }
+    }
     render(){
         return(
             <div className="app">
             
-                <div className="timer">10</div>
+                <div className="timer">{this.state.count}</div>
             <div style={{textAlign: 'center'}}>
-            <button className="btn">-</button>
-            <button className="btn">+</button>
+            <button onClick={this.handleDecrement} className="btn">-</button>
+            <button  onClick={this.handleIncrement} className="btn">+</button>
             </div>
             </div>
         )
